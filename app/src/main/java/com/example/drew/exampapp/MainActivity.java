9f7a6caeb280a2026e8acce7120b75ffbfe6cb7c
DrewGaard <d_overgaard1@u.pacific.edu>
2018-06-21 18:04:45 -0700

    Creating a simple login screen

--- app/src/main/java/com/example/drew/exampapp/MainActivity.java
+++ app/src/main/java/com/example/drew/exampapp/MainActivity.java
@@ -4,11 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
+
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
         Log.i("Info", "Button pressed!");
+        Log.i("Name", nameEditText.getText().toString());
+        Log.i("Password", passwordEditText.getText().toString());
     }
 
     @Override
