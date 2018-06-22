4ab73f1d43d4ea2b26707c6783e797c1e80f248c
DrewGaard <d_overgaard1@u.pacific.edu>
2018-06-21 20:42:34 -0700

    Change image on button press

--- app/src/main/java/com/example/drew/exampapp/MainActivity.java
+++ app/src/main/java/com/example/drew/exampapp/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Password", passwordEditText.getText().toString());
 
         Toast.makeText(this,"Hello " + nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void catClick(View view) {
+
+        ImageView catImage = (ImageView) findViewById(R.id.imageView);
+        Button catButton = (Button) findViewById(R.id.catButton);
+
+        catImage.setImageResource(R.drawable.cat2);
     }
 
     @Override
